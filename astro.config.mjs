4cacebe9df7549252e0abb7d3c4cdc38b005b802
cliff <clifflee1022@gmail.com>
2024-04-22 22:36:42 +0800

    feat: 6.1 Build your first Astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cliff.zeabur.app"
+  site: "https://cliff.zeabur.app",
+  integrations: [preact()]
 });
